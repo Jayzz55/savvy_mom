7deb0d43e0150df39eb7f191685e66e743d17206
jayzz55 <jayzzwijono@yahoo.com>
2015-03-16 12:46:17 +1100

    test puffing-billy

--- spec/features/testing_puffing_billy_spec.rb
+++ spec/features/testing_puffing_billy_spec.rb
@@ -8,12 +8,13 @@ feature 'testing puffing billy', js: true do
     # visit 'http://example.com/text/'
     # expect(page).to have_content("Foobar")
     require './app/jobs/check_catalogues_job.rb'
-    proxy.stub("http://salefinder.com.au/Coles-catalogue/").and_return(:text => 'Foobar')
-    proxy.stub("http://salefinder.com.au/Woolworths-catalogue/").and_return(:text => 'Foobar')
+    # proxy.stub("http://salefinder.com.au/Coles-catalogue/")
+    # proxy.stub("http://salefinder.com.au/Woolworths-catalogue/")
     # # CheckCataloguesJob.perform_later
     # CheckCataloguesJob.new.scrape_published_catalogue_nums
     visit "http://salefinder.com.au/Coles-catalogue/"
-    expect(page).to have_content("Foobar")
+    visit "http://salefinder.com.au/Woolworths-catalogue/"
+    # expect(page).to have_content("Foobar")
 
   end
 end
