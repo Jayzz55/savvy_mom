0cbe6c4c79ff5f3c557c73bcd5c5aeb00d851a5b
jayzz55 <jayzzwijono@yahoo.com>
2015-03-24 00:46:06 +1100

    fix post controller query to return all associated items using where rather than find_by

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,8 @@ class PostsController < ApplicationController
     @posts = Post.all.paginate(:page => params[:page], :per_page => 20)
     @posts = @posts.where("saving >= ?", params["min_saving"].to_i) if params["min_saving"].present?
     @posts = @posts.where("saving_percentage >= ?", params["min_saving_percentage"].to_i) if params["min_saving_percentage"].present?
-    @posts = @posts.where(catalogue_id: Catalogue.find_by(shop:params["coles"]).id) if params["coles"].present? && !(params["woolworths"].present?)
-    @posts = @posts.where(catalogue_id: Catalogue.find_by(shop:params["woolworths"]).id) if params["woolworths"].present? && !(params["coles"].present?)
-    @posts = @posts.where(catalogue_id: Catalogue.find_by(date:params["period"]).id) if params["period"].present?
+    @posts = @posts.where(catalogue_id: Catalogue.where(shop:params["coles"]).pluck(:id)) if params["coles"].present? && !(params["woolworths"].present?)
+    @posts = @posts.where(catalogue_id: Catalogue.where(shop:params["woolworths"]).pluck(:id)) if params["woolworths"].present? && !(params["coles"].present?)
+    @posts = @posts.where(catalogue_id: Catalogue.where(date:params["period"]).pluck(:id)) if params["period"].present?
   end
 end
